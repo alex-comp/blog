3360851fe093083d414638b952bcd7ad34aafa83
Alex.comp <alexandresantos.nascimento@saude.se.gov.br>
2021-03-27 20:36:31 -0300

    feat: Service criada

---
+++ src/main/java/com/spring/blog/service/serviceImpl/BlogServiceImpl.java
@@ -0,0 +1,30 @@
+package com.spring.blog.service.serviceImpl;
+
+import com.spring.blog.model.Post;
+import com.spring.blog.repository.BlogRepository;
+import com.spring.blog.service.BlogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BlogServiceImpl implements BlogService {
+    @Autowired
+    BlogRepository blogRepository;
+
+    @Override
+    public List<Post> findAll() {
+        return findAll();
+    }
+
+    @Override
+    public Post findById(long id) {
+        return blogRepository.findById(id).get();
+    }
+
+    @Override
+    public Post save(Post post) {
+        return blogRepository.save(post);
+    }
+}
