83e1ad2bd549db78bf32642e551243e038f05918
Alex.comp <alexandresantos.nascimento@saude.se.gov.br>
2021-05-15 21:25:26 -0300

    feat: paginaçao da lista de usuarios

--- src/main/java/com/spring/blog/utils/DummyData.java
+++ src/main/java/com/spring/blog/utils/DummyData.java
@@ -31,8 +31,8 @@ public class DummyData {
     @Autowired
     GrupoPermissaoService grupoPermissaoService;
 
-    //    @PostConstruct
-    public void createAdminUser(){
+//    @PostConstruct
+    public void createAdminUser() {
         //Criando usuario
         Usuario usuario = new Usuario();
         usuario.setAtivo(true);
@@ -49,65 +49,9 @@ public class DummyData {
         usuario2.setSenha(new BCryptPasswordEncoder().encode("test"));
         usuarioService.save(usuario2);
 
-        Usuario usuario3 = new Usuario();
-        usuario3.setAtivo(true);
-        usuario3.setNome("Test3");
-        usuario3.setLogin("test3");
-        usuario3.setSenha(new BCryptPasswordEncoder().encode("test"));
-        usuarioService.save(usuario3);
-
-        Usuario usuario4 = new Usuario();
-        usuario4.setAtivo(true);
-        usuario4.setNome("Test4");
-        usuario4.setLogin("test4");
-        usuario4.setSenha(new BCryptPasswordEncoder().encode("test"));
-        usuarioService.save(usuario4);
-
-        Usuario usuario5 = new Usuario();
-        usuario5.setAtivo(true);
-        usuario5.setNome("Test5");
-        usuario5.setLogin("test5");
-        usuario5.setSenha(new BCryptPasswordEncoder().encode("test"));
-        usuarioService.save(usuario5);
-
-        Usuario usuario6 = new Usuario();
-        usuario6.setAtivo(true);
-        usuario6.setNome("Test6");
-        usuario6.setLogin("test6");
-        usuario6.setSenha(new BCryptPasswordEncoder().encode("test"));
-        usuarioService.save(usuario6);
-
-        Usuario usuario7 = new Usuario();
-        usuario7.setAtivo(true);
-        usuario7.setNome("Test7");
-        usuario7.setLogin("test7");
-        usuario7.setSenha(new BCryptPasswordEncoder().encode("test"));
-        usuarioService.save(usuario7);
-
-        Usuario usuario8 = new Usuario();
-        usuario8.setAtivo(true);
-        usuario8.setNome("Test8");
-        usuario8.setLogin("test8");
-        usuario8.setSenha(new BCryptPasswordEncoder().encode("test"));
-        usuarioService.save(usuario8);
-
-        Usuario usuario9 = new Usuario();
-        usuario9.setAtivo(true);
-        usuario9.setNome("Test9");
-        usuario9.setLogin("test9");
-        usuario9.setSenha(new BCryptPasswordEncoder().encode("test"));
-        usuarioService.save(usuario9);
-
-        Usuario usuario10 = new Usuario();
-        usuario10.setAtivo(true);
-        usuario10.setNome("Test10");
-        usuario10.setLogin("test10");
-        usuario10.setSenha(new BCryptPasswordEncoder().encode("test"));
-        usuarioService.save(usuario10);
-
         //Criando permissao
         Permissao permissao = new Permissao();
-        permissao.setNome("CRIAR_POST");
+        permissao.setNome("ROLE_CRIAR_POST");
         permissao.setDescricao("Criação de Posts");
         permissao = permissaoService.save(permissao);
 
